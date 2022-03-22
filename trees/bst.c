25a6fa3abaf716623111cc5b823011f7bf7596a4
akdas75 <dasajoy80@gmail.com>
2022-03-22 12:59:23 +0530

    All programs done for bst

--- trees/bst.c
+++ trees/bst.c
@@ -2,17 +2,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 typedef unsigned int uint32_t;
 
 typedef struct  bst_t {
-
     struct bst_t * left;
-    uint32_t data;
+    int32_t data;
     struct bst_t * right;
-
-
 } bst;
+
+typedef  struct queue_t {
+    struct bst_t **data;
+    int rear;
+    int front;
+    int size;
+}queue;
+
+
+
+int max (int a, int b) {
+
+    return a > b ? a : b;
+}
+
+int min (int a, int b) {
+
+    return a > b ? b : a;
+}
 
 
 bst*  bst_insert (bst *root, uint32_t element) {
@@ -107,7 +124,7 @@ bst* minValueNode(bst* node)
         current = current->left;
  
     printf ("Inorder succesor %d \n",current->data);
-    
+
     return current;
 }
 
@@ -163,17 +180,254 @@ bst* bst_delete (bst *root, int element) {
 }
 
 
+int bst_height (bst *root) {
+
+    unsigned int lHeight = 0;
+    unsigned int rHeight = 0;
+    
+    if (root == NULL) {
+        return 0;
+    } 
+
+
+    lHeight = bst_height (root->left);
+    rHeight = bst_height (root->right);
+
+    int mH = max (lHeight, rHeight) + 1;
+    printf ("Returning node %d height %d \n",root->data,mH);
+    return mH;
+
+}
+
+int bst_min_height (bst *root) {
+
+    unsigned int lHeight = 0;
+    unsigned int rHeight = 0;
+    
+    if (root == NULL) {
+        return 0;       
+    } 
+
+    if ((root->left == NULL)  && (root->right == NULL)) {
+        return 1;       
+    } 
+
+
+    lHeight = bst_min_height (root->left);
+    rHeight = bst_min_height (root->right);
+
+    int mH = min (lHeight, rHeight) + 1;
+    //printf ("Returning min height %d \n",mH);
+    printf ("Returning node %d l %d r %d min height %d \n",root->data, lHeight, rHeight, mH);
+
+    return mH;
+
+}
+
+
+int bst_count_nodes (bst *root) {
+
+    if (root == NULL) {
+        return 0;       
+    } 
+
+    int count = bst_count_nodes (root->left) + bst_count_nodes (root->right) + 1;
+
+    return  count;
+
+}    
+
+int bst_count_leaf_nodes (bst *root) {
+
+    if (root == NULL) {
+        return 0;       
+    } 
+
+    if ((root->left == NULL) && (root->right == NULL)) {
+        return 1;
+    }
+
+    int count = bst_count_leaf_nodes (root->left) + bst_count_leaf_nodes (root->right);
+
+    return  count;
+
+}  
+
+
+int bst_count_non_leaf_nodes (bst *root) {
+
+    if (root == NULL) {
+        return 0;       
+    } 
+
+    /*leaf node */
+
+    if ((root->left == NULL) && (root->right == NULL)) {
+        return 0;
+    }
+
+    int count = bst_count_non_leaf_nodes (root->left) + bst_count_non_leaf_nodes (root->right) + 1;
+
+    printf ("Node %d count %d \n",root->data, count);
+
+    return  count;
+
+} 
+
+
+int check_if_bst (bst *root , int min, int max) {
+
+      if (root == NULL) {
+          return 1;
+      }
+
+      printf ("data %d min %d max %d \n",root->data, min,max);
+
+      if (root->data > max) {
+          printf ("Case 1 \n");
+      }
+
+      if (root->data < min) {
+          printf ("Case 2 \n");
+      }
+
+      /* False case */
+      if ((root->data > max)  || (root->data < min)) {
+          printf ("Not sataisfying returning \n");
+          return 0;
+      }
+
+      return (check_if_bst (root->left, min,root->data-1) &&
+      check_if_bst (root->right, root->data+1,max));
+
+
+}
+
+
+void init_queue (queue **q, int size) {
+
+    *q = (queue *)malloc (sizeof (struct queue_t));
+
+    (*q)->rear = 0;
+    (*q)->front = 0;
+    (*q)->size = size;
+
+    (*q)->data = (bst**)malloc (sizeof (bst*) * size);
+
+}
+
+void enqueue (queue **q, struct bst_t* ptr) {
+
+
+    int rear = (*q)->rear;
+    int size = (*q)->size;
+    int front = (*q)->front;    
+
+    if (((rear +1) % size) == front) {
+        printf ("Queue is full \n");
+    } else {
+        printf ("Queueing element at rear %d \n",rear);
+        rear = rear + 1;    
+        rear = rear % size;
+        (*q)->rear =  rear;
+         printf ("Queueing element at rear %d \n",rear);
+        (*q)->data [rear] = ptr;
+    }    
+}
+
+bst* dequeue (queue **q) {   
+
+     int rear = (*q)->rear;
+    int size = (*q)->size;
+    int front = (*q)->front;
+
+    if (rear == front) {
+        printf ("Queue is empty \n");
+        return NULL;
+    } else {
+        front = front + 1;
+        front = front % size;
+         printf ("Dequeing element at rear %d \n",front);
+        (*q)->front = front;
+        return (*q)->data [front];
+    }  
+    
+    return NULL;
+}
+
+void printLevelOrder (queue **q, bst *root) {
+
+    bst *temp_node = root;
+
+    while (temp_node != NULL) {
+
+        printf("************************************ %d \n", temp_node->data);
+
+        if (temp_node->left)
+            enqueue(q,  temp_node->left);
+
+        if (temp_node->right)
+            enqueue(q, temp_node->right);    
+
+        /*Dequeue node and make it temp_node*/
+        temp_node = dequeue(q);
+    }
+}
+
 int main (void) {
 
     bst *root = NULL;
-
-    root = bst_insert (root, 30);
+    root = bst_insert (root, 15);
+    root = bst_insert (root, 10);
     root = bst_insert (root, 20);
-    root = bst_insert (root, 40);
-    root = bst_insert (root, 70);
-    root = bst_insert (root, 60);
-    root = bst_insert (root, 80);
+    root = bst_insert (root, 8);
+    root = bst_insert (root, 12);
+    root = bst_insert (root, 16);
+    root = bst_insert (root, 25);
 
+
+/*
+      root = bst_insert (root, 3);
+    root = bst_insert (root, 2);
+    root = bst_insert (root, 20);
+    root = bst_insert (root, 15);    
+    root = bst_insert (root, 25);
+*/
+
+/*
+    root = bst_insert (root, 1);
+    root = bst_insert (root, 2);
+    root = bst_insert (root, 3);
+    root = bst_insert (root, 4);
+    root = bst_insert (root, 5);
+    root = bst_insert (root, 6);
+    root = bst_insert (root, 8);
+    root = bst_insert (root, 7);
+*/
+
+    int h = bst_height (root);
+    printf ("Height of the tree %d \n",h);
+
+    int mh = bst_min_height (root);
+    printf ("Min Height of the tree %d \n",mh);
+
+
+    int ct = bst_count_nodes (root);
+    printf ("Node count of the tree %d \n",ct);
+
+    
+
+    int lct = bst_count_leaf_nodes (root);
+    printf ("leaf Node count of the tree %d \n",lct);
+
+    
+
+    int nlct = bst_count_non_leaf_nodes (root);
+    printf ("Non leaf Node count of the tree %d \n",nlct);
+
+
+    int ifBst = check_if_bst (root, INT_MIN, INT_MAX);
+    printf ("if BST %d \n",ifBst);
 
     printf ("Displaying inorder ..... \n");
       
@@ -200,6 +454,13 @@ int main (void) {
     printf ("Displaying inorder ..... \n");
       
     bst_inorder (root);
+
+    queue *q;
+    init_queue (&q , 100);
+
+    printf ("Displaying level order \n");
+
+    printLevelOrder (&q, root);
     
 
 }
